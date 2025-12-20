d18076f9aea30cdc8129c956d2f45123791f1439
NigelId <suon568@gmail.com>
2025-12-20 19:41:06 +0700

    mooooooooooooooooooa.

--- dotfiles/.config/nvim/lua/plugins/tree-sitter.lua
+++ dotfiles/.config/nvim/lua/plugins/tree-sitter.lua
@@ -4,6 +4,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	event = { "BufReadPost", "BufNewFile" },
+   branch="master",
 	lazy = false,
 	dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
 	config = function()
